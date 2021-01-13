252e1a6e8e036e6c605d2b54767c2f5cf324cce2
PatriceJiang <397136899@qq.com>
2021-01-13 16:47:53 +0800

    init commit

---
+++ x86_64/lib/cmake/libwebsockets/LibwebsocketsTargets-release.cmake
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "websockets" for configuration "Release"
+set_property(TARGET websockets APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(websockets PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "/Users/pt/Library/Huawei/Sdk/native/2.1.0.5/sysroot/usr/lib/aarch64-linux-ohos/libz.so;/Volumes/data/hw/x86_64/lib/libssl.a;/Volumes/data/hw/x86_64/lib/libcrypto.a;/Volumes/data/hw/x86_64/lib/libuv_a.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libwebsockets.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS websockets )
+list(APPEND _IMPORT_CHECK_FILES_FOR_websockets "${_IMPORT_PREFIX}/lib/libwebsockets.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
